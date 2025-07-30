f7a0ecb66eeca0e31e5eac9fb642c6fccd44be19
Susan Tang <130117000+susantang-nz@users.noreply.github.com>
2025-07-31 07:26:25 +1200

    turn useMockData off

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const app = express();
 // const port = 3000;
 const port = process.env.PORT || 3000;
 
-const useMockData = true;
+const useMockData = false;
 
 app.use(express.static("public"))
 
